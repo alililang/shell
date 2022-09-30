78d0f9881cda0d9e960f7d311f6aae880911c038
alililang <41756473+alililang@users.noreply.github.com>
2022-10-01 01:16:01 +0800

    Create updatentp.sh
    Centos7更新ntp和ntpd

---
+++ updatentp.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+red='\033[31m'
+green='\033[32m'
+yellow='\033[33m'
+plain='\033[0m'
+echo -e "${green}#########################################"
+echo -e "${green}#                                       #"
+echo -e "${green}#         jupyter一键部署脚本           #"
+echo -e "${green}#  此脚本由aliase@aliyun.com维护        #"
+echo -e "${green}#           此脚本仅用于学习            #"
+echo -e "${green}#                                       #"
+echo -e "${green}#########################################${plain}"
+#wget http://www.eecis.udel.edu/~ntp/ntp_spool/ntp4/ntp-4.2/ntp-4.2.8p15.tar.gz
+yum -y install gcc gcc-c++ openssl-devel libstdc++* libcap*
+cp -ar /etc/ntp /etc/ntp.bak
+cp /etc/ntp.conf /etc/ntp.conf.bak
+cp /etc/sysconfig/ntpd /etc/sysconfig/ntpd.bak
+cp /etc/sysconfig/ntpdate /etc/sysconfig/ntpdate.bak
+yum -y remove ntp ntpdate
+tar xvf ntp-4.2.8p15.tar.gz
+cd ntp-4.2.8p15/
+mkdir /usr/share/doc/ntp-4.2.8p15
+./configure \
+--prefix=/usr \
+--bindir=/usr/sbin \
+--sysconfdir=/etc \
+--enable-all-clocks \
+--enable-parse-clocks \
+--docdir=/usr/share/doc/ntp-4.2.8p15
+make clean
+make -j8
+make install
+ln -s /usr/local/ntp-4.2.8p15 /usr/local/ntp
+cp /etc/ntp.conf.bak /etc/ntp.conf
+cp /etc/ntp/keys.rpmsave /etc/ntp/keys
+cp /usr/sbin/ntpd /etc/init.d/ntpd
+/usr/sbin/ntpd -c /etc/ntp.conf
+ntpd --version
+Current_version=$(ntpd --version 2>&1)
+Successful_version="ntpd 4.2.8p15"
+
+if [[ $Current_version =~ $Successful_version ]]; then echo -e "${green} 升级成功 ${plain}";
+   else
+   echo -e "${red}升级失败 ${plain}"; exit 1;
+fi
